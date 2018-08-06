c4673642238b828298c4e56f1dbab2a8c03b4dd4
xhansana <hansana@hotmail.co.th>
2018-08-06 13:45:52 +0700

    Initial commit

---
+++ app/src/main/java/chill_order/com/laosunseen/MainActivity.java
@@ -0,0 +1,25 @@
+package chill_order.com.laosunseen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import chill_order.com.laosunseen.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //add Fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new MainFragment())
+                    .commit();
+        }
+
+    }   //Main Method
+}   //Main Class
---
+++ app/src/main/java/chill_order/com/laosunseen/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package chill_order.com.laosunseen.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import chill_order.com.laosunseen.R;
+
+public class MainFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+
+    }
+}
