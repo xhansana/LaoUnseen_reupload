7ab5872d946e0624b011e4080da89a8e9821a83e
xhansana <hansana@hotmail.co.th>
2018-08-07 16:58:34 +0700

    Cloud Upload Icon

--- app/src/main/java/chill_order/com/laosunseen/fragment/RegisterFragment.java
+++ app/src/main/java/chill_order/com/laosunseen/fragment/RegisterFragment.java
@@ -10,6 +10,9 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -23,6 +26,28 @@ public class RegisterFragment extends Fragment {
     //            Explicit
     private Uri uri;
     private ImageView imageView;
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+
+        inflater.inflate(R.menu.menu_register, menu);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+
+        if (item.getItemId() == R.id.itemUpload) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -85,6 +110,7 @@ public class RegisterFragment extends Fragment {
                 getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+        setHasOptionsMenu(true);
 
     }
 
